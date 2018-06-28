476793ea6710cef5863edf8d1c2fa6cfdde495d1
Felipe Silva <felipeko@gmail.com>
2018-06-28 15:59:41 -0300

    add logged wishlist and carts

--- client/src/reducers/actions.js
+++ client/src/reducers/actions.js
@@ -10,7 +10,7 @@ export const CHANGE_QUANTITY  = 'CHANGE_QUANTITY'
 export const login = (payload) => ({type:LOGIN, payload})
 export const logout = () => ({type:LOGOUT})
 
-export const initCart = (cart,id) => ({type:INIT_CART, payload:{...cart,id}})
+export const initCart = (cart) => ({type:INIT_CART, payload:cart})
 export const addProduct = (id,quantity) => ({type:ADD_PRODUCT_TO_CART, id,quantity})
 export const removeProduct = (id) => ({type:REMOVE_PRODUCT_FROM_CART, id})
 export const changeQuantity = (id,quantity) => ({type:REMOVE_PRODUCT_FROM_CART, id, quantity})
